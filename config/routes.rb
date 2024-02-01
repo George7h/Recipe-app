0f31500898ef4902abaa58db78815bb4978f4c99
George <georgie.george7@gmail.com>
2024-02-01 18:47:05 +0400

    Add public recipes feature and route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get '/public_recipes', to: 'recipes#public_recipes', as: 'public_recipes'
+  
 devise_scope :user do
   authenticated :user do
     root :to => 'foods#index', as: :authenticated_root
@@ -13,7 +14,9 @@ end
 
 resources :foods, only: [:index, :new, :create, :destroy]
 resources :shopping_lists, only: [:index]
-resources :recipes
+resources :recipes, only: [:index, :new, :create, :show, :update, :destroy] do
+  resources :recipe_foods
+end
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
