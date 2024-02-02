8ef62c90c08a5efca02c7b5b40382b2d94354ed8
George <georgie.george7@gmail.com>
2024-02-02 17:54:23 +0400

    Update description length validation in Recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,7 +4,7 @@ class Recipe < ApplicationRecord
   has_many :recipe_foods
 
   validates :name, presence: true
-  validates :description, length: { minimum: 500 }
+  validates :description, length: { maximum: 6000 }
   validates :preparation_time, numericality: { greater_than_or_equal_to: 0 }
   validates :cooking_time, numericality: { greater_than_or_equal_to: 0 }
 end
